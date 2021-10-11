682ca47b907b7e5808651936c56f2269a54861f7
Liliya Siadzelnikava <sidelnikova.liliya@gmail.com>
2021-10-11 10:08:20 +0300

    Mapper interface from client model to entity and otherwise created: 'TagClientEntityModelMapper'.  Mapper interface from client model to entity and otherwise created for table 'Tag'

---
+++ service/src/main/java/com/epam/esm/mapper/TagClientEntityModelMapper.java
@@ -0,0 +1,11 @@
+package com.epam.esm.mapper;
+
+import com.epam.esm.model.TagClientModel;
+import com.epam.esm.model.TagEntityModel;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface TagClientEntityModelMapper {
+    TagClientModel entityToClient(TagEntityModel tagEntityModel);
+    TagEntityModel clientToEntity(TagClientModel tagClientModel);
+}
